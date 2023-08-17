3ae780a14da4007dd0dc6291d4a5d38317d093a2
Ashraf-2 <hasanashraful03@gmail.com>
2023-08-17 23:09:05 +0600

    deposit done

--- deposit.js
+++ deposit.js
@@ -4,16 +4,48 @@ document.getElementById('depo-btn').addEventListener('click',function(){
 
     // step-2: get the deposit amount from the deposit field
     const depositField = document.getElementById('depo-field');
-    const depositAmount = depositField.value;
-    // console.log(depositAmount);
+    const newDepositAmount_string = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmount_string);
+    // console.log(newDepositAmount);
+
+    depositField.value = '';
+
+    //parseFloat --->  convert string to float type.
 
     //step-3: get the current deposit total amount
     const depositTotalElement = document.getElementById('deposit_total');
-    const depositTotal = depositTotalElement.innerText;
+    const prevDepositTotal_string = depositTotalElement.innerText;
+    const prevDepositTotal = parseFloat(prevDepositTotal_string);
     // console.log(depositTotal);
-    depositTotalElement.innerText = depositTotal + depositAmount;
-    console.log(depositTotal)
+    // console.log(prevDepositTotal)
 
+    //step-4: set the total amount of deposit value;
+    const currentDepositTotal = prevDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+
+    //step-5: adjust the total balance amount;
+    const balanceTotalElement = document.getElementById('balance_total');
+    const prevBalanceTotal_string = balanceTotalElement.innerText;
+    const prevBalanceTotal = parseFloat(prevBalanceTotal_string);
+    
+    //step-6: calculate total balance
+    const currentBalanceTotal = prevBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+    // console.log("final",balanceTotalElement);
+
+
+
+    
+
+
+
+
+
+
+
+    //step-7: clear the deposit field
+    depositField.value = '';
 
 
    
