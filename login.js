c6be0b43cc1bfbd4cc005da7be7d878f5db9f0dd
Ashraf-2 <hasanashraful03@gmail.com>
2023-08-17 20:38:42 +0600

    added design and bank page

---
+++ login.js
@@ -0,0 +1,28 @@
+
+// console.log('login in js file');
+// step-1: add click event handler with the submit button
+document.getElementById('submit-btn').addEventListener('click',function(){
+    // step-2: get the email address inside the email input field
+    // console.log('submit botton clicked');
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3: get password
+    // 3.a: set id on the html element
+    // 3.b: get the element
+    // 3.c: get the value form the element
+    const passField = document.getElementById('user-pass');
+    const pass = passField.value;
+    // console.log(email, pass);
+
+    // step-4: verify email and password.
+    if(email === 'houmaukau@gmail.com' && pass === 'secretBoss')
+    {
+        console.log('valid user');
+    }
+    else
+    {
+        console.log('invaild user');
+    }
+
+    // DANGER: do not varify email and password on client side.
+})
